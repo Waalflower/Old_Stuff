d8f9882194f8535b174b99aa04984c674924bf22
Waalflower <sbzmtg@hotmail.com>
2016-08-31 20:03:03 -0700

    Update hello_friend.py

--- hello_friend.py
+++ hello_friend.py
@@ -1,5 +1,5 @@
 #Stupid little practice lesson to say hi to friends. 8/31/2016
 names = ['Aaron', 'Jesse', 'Josh', 'Lance', 'Yos']
-greetings = ['Hola', 'Hello', 'Hi', 'Howdy', 'Sup']
+greetings = ['hola', 'hello', 'hi', 'howdy', 'sup']
 
 print(greetings[2].title() + " " + names[1])
